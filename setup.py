cc3c44df759c9ff412682143519054c14a6656ff
Enes Esvet Kuzucu <eneskuzucu@hotmail.com>
2024-10-16 13:11:29 +0300

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 setup(
     # name='LLM',  # Package name
     name='llmservice',  # Package name
-    version='0.0.9',  # Version of your package
+    version='0.1.0',  # Version of your package
     author='Enes Kuzucu',  # Your name
 
     description='for creating fast and easy llm apps',  # Short description
